cc42840093b0b219ad01f1d14cdfa10ee6998f60
Kirill Che <kot313@gmail.com>
2022-09-25 18:59:32 +0300

    add cli

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,64 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/c-bata/go-prompt"
+)
+
+func executor(in string) {
+	in = strings.TrimSpace(in)
+	blocks := strings.Split(in, " ")
+	switch blocks[0] {
+	case "exit":
+		fmt.Println("GophKeeper stop")
+		os.Exit(0)
+		return
+	case "reg":
+		fmt.Println("i input reg")
+	case "auth":
+		fmt.Println("i input auth")
+	}
+}
+
+func completer(d prompt.Document) []prompt.Suggest {
+	var s []prompt.Suggest
+
+	switch d.Text {
+	case "reg":
+		s = []prompt.Suggest{
+			{Text: "reg", Description: "user registration"},
+		}
+	case "auth":
+		s = []prompt.Suggest{
+			{Text: "auth", Description: "user authentication"},
+		}
+	case "exit":
+		s = []prompt.Suggest{
+			{Text: "exit", Description: "exit program"},
+		}
+	default:
+		s = []prompt.Suggest{
+			{Text: "reg", Description: "user registration"},
+			{Text: "auth", Description: "user authentication"},
+			{Text: "exit", Description: "exit program"},
+		}
+	}
+
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func main() {
+	fmt.Println("GophKeeper start")
+
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionTitle("menu"),
+		prompt.OptionPrefix(">>> "),
+		prompt.OptionInputTextColor(prompt.Green),
+	)
+	p.Run()
 }
